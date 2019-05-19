0e8307ebcabfa4733f6268f3e913fa58ffa696b2
roy11777 <46893192+roy11777@users.noreply.github.com>
2019-05-20 00:13:15 +0800

    0520 morning Forum Read

---
+++ src/component/Forum/ForumArticleComment/ForumCommentAvatarRoy.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ForumArticleAvatarRoy = props => {
+  return (
+    <>
+      <div className="media d-flex align-items-center">
+        <div
+          className="rounded-circle overflow-hidden"
+          style={{ width: '75px', height: '75px' }}
+        >
+          <img
+            src={'http://localhost:3000/images/' + props.commentAvatar + '.jpg'}
+            className="h-100 w-100 align-self-center mr-3"
+            alt=""
+          />
+        </div>
+        <div className="media-body">
+          <p className="text-light m-3">{props.commentUserName}</p>
+          <p className="text-light m-3">一分鐘前</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ForumArticleAvatarRoy
