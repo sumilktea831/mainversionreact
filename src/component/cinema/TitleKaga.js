3b1668a28d5e65f43aabb324dde9a4e5fe62c224
jlc7191 <44053726+jlc7191@users.noreply.github.com>
2019-05-25 23:58:52 +0800

    衝一波試試看

---
+++ src/component/cinema/TitleKaga.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TitleKaga = props => (
+  <>
+    <span
+      style={{
+        borderBottom: '2px solid #d4d1cc',
+        fontSize: '28px',
+      }}
+    >
+      {props.title}
+    </span>
+  </>
+)
+
+export default TitleKaga
