63ac9a2578ac90d1b10aa82d4a2e6587869cf4ce
NexonL <nexon0201@gmail.com>
2019-05-29 16:01:42 +0800

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ class App extends React.Component {
             <Route path="/forum/:id" component={Forum} />
             <Route path="/forum" component={Forum} />
             <Route path="/LoginSign" component={LoginSign} />
-            <Route path="/BackMainpage" component={Mainpage} />
+            <Route path="/BackMainpage" component={BackMainpage} />
             <Route path="/CinemaBackMainpage" component={CinemaBackMainpage} />
           </Switch>
           <div className="container-fluid" style={{}}>
